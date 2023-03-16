bd4953636a8173b4dfae2b6e03a7e0c5a6c63715
lady <lady@udenar.edu.co>
2023-03-16 10:51:46 -0500

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,63 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/TodoReducer';
+
+
+
+
+const init = () => {
+
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+
+
+    //funcion que hay que hacer para emitir el todo revisar lo que sale en el console
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] add todo',
+            payload: todo
+        }
+        //para mandar mi acción utilizo el dispatch
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+
+        dispatch({
+            type: '[TODO] remove todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+
+        dispatch({
+            type: '[TODO] toggle todo',
+            payload: id
+        });
+
+       
+    }
+
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount:todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+       
+    }
+
+
+}
